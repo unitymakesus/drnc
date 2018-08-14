1970505e8cd8896c57fb2f948f65e0ee8b2672f8
lexinamer <lexinamer@gmail.com>
2018-08-14 14:58:06 -0400

    Fixing En espanol feature on tools page

--- wp-content/themes/drnc/resources/views/template-tools.blade.php
+++ wp-content/themes/drnc/resources/views/template-tools.blade.php
@@ -7,7 +7,9 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
-    @include('partials.content-page')
+    <div class="content-container">
+      @include('partials.content-page')
+    </div>
   @endwhile
 
   <section class="border-top" role="region" aria-label="Resources List">

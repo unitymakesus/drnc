ef3428e1767d633382e9e5d8aa137c75a6f52510
Alisa Herr <alisa@unitymakes.us>
2018-11-30 14:06:58 -0500

    Remove unnecessary links from board member admin bar

--- wp-content/themes/drnc/app/admin.php
+++ wp-content/themes/drnc/app/admin.php
@@ -22,3 +22,15 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * Cleanup WP Admin bar for Board Members
+ */
+add_action( 'wp_before_admin_bar_render', function() {
+  global $wp_admin_bar;
+  if (!current_user_can('manage_options')) {  // If user can't manage options (ie is an Administrator) remove menu items
+    $wp_admin_bar->remove_menu('mc-my-calendar');
+    $wp_admin_bar->remove_menu('search');
+    $wp_admin_bar->remove_menu('wp-logo');
+  }
+});
